d79573240731d52c9623641f13265ff69aabd15a
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-04-15 14:05:13 +0800

    add data model

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by ChihYu Yeh on 2019/4/15.
+//  Copyright © 2019 cyyeh. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+  
+  func toggleChecked() {
+    checked = !checked
+  }
+}
