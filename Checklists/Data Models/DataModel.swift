679186b87b807f018d20e40760e0131b41aecc08
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-04-15 21:38:38 +0800

    user defaults

--- Checklists/Data Models/DataModel.swift
+++ Checklists/Data Models/DataModel.swift
@@ -10,9 +10,20 @@ import Foundation
 
 class DataModel {
   var lists = [Checklist]()
+  var indexOfSelectedChecklist: Int {
+    get {
+      return UserDefaults.standard.integer(forKey: "ChecklistIndex")
+    }
+    set {
+      UserDefaults.standard.set(newValue, forKey: "ChecklistIndex")
+      UserDefaults.standard.synchronize()
+    }
+  }
   
   init() {
     loadChecklists()
+    registerDefaults()
+    handleFirstTime()
   }
   
   // MARK:- Data Persistence
@@ -45,5 +56,24 @@ class DataModel {
         print("Error decoding list array: \(error.localizedDescription)")
       }
     }
+  }
+  
+  func handleFirstTime() {
+    let userDefaults = UserDefaults.standard
+    let firstTime = userDefaults.bool(forKey: "FirstTime")
+    
+    if firstTime {
+      let checklist = Checklist(name: "List")
+      lists.append(checklist)
+      
+      indexOfSelectedChecklist = 0
+      userDefaults.set(false, forKey: "FirstTime")
+      userDefaults.synchronize()
+    }
+  }
+  
+  func registerDefaults() {
+    let dictionary = [ "ChecklistIndex": -1, "FirstTime": true ] as [String : Any]
+    UserDefaults.standard.register(defaults: dictionary)
   }
 }
--- Checklists/View Controllers/AllListsViewController.swift
+++ Checklists/View Controllers/AllListsViewController.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
+class AllListsViewController: UITableViewController,
+                              ListDetailViewControllerDelegate,
+                              UINavigationControllerDelegate {
   let cellIdentifier = "ChecklistCell"
   var dataModel: DataModel!
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    navigationController?.delegate = self
+    
+    let index = dataModel.indexOfSelectedChecklist
+    if index >= 0 && index < dataModel.lists.count {
+      let checklist = dataModel.lists[index]
+      performSegue(withIdentifier: "ShowChecklist", sender: checklist)
+    }
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -34,6 +48,7 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    dataModel.indexOfSelectedChecklist = indexPath.row
     let checklist = dataModel.lists[indexPath.row]
     performSegue(withIdentifier: "ShowChecklist", sender: checklist)
   }
@@ -63,6 +78,13 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     } else if segue.identifier == "AddChecklist" {
       let controller = segue.destination as! ListDetailViewController
       controller.delegate = self
+    }
+  }
+  
+  // MARK:- Navigation Controller Delegate
+  func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+    if viewController === self {
+      dataModel.indexOfSelectedChecklist = -1
     }
   }
   
